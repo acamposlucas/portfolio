e18342ea2cfa4702978b5cd98d3c67725b85f299
lucas <lac.lucas96@gmail.com>
2022-09-15 18:09:03 -0300

    feat: create ProfileCard component

---
+++ src/components/ProfileCard.tsx
@@ -0,0 +1,52 @@
+import { LinkedinLogo, GithubLogo } from "phosphor-react";
+
+export const ProfileCard = () => {
+	return (
+		<div className="flex flex-col w-full gap-4 items-center bg-slate-300 text-zinc-900 opacity-75 rounded-lg py-4">
+			<img
+				className="w-48 h-48 rounded-full -mt-10"
+				src="https://github.com/acamposlucas.png"
+				alt="Lucas"
+			/>
+			<div className="flex flex-col gap-2 items-center w-4/5 after:h-0.5 after:w-full after:block after:bg-slate-900 after:opacity-50">
+				<h1 className="text-2xl font-bold font-heading">Lucas Almeida</h1>
+				<strong className="font-normal">Software Engineer</strong>
+				<p className="text-center">
+					Coder, swimmer, historian. None of those activities made me someone
+					important, but I'm ok with that.
+				</p>
+			</div>
+			<ul className="flex items-center justify-center gap-2 flex-wrap">
+				<li className="px-4 py-2 bg-red-400 rounded-lg">JavaScript</li>
+				<li className="px-4 py-2 bg-red-400 rounded-lg">React</li>
+				<li className="px-4 py-2 bg-red-400 rounded-lg">TypeScript</li>
+			</ul>
+			<div>
+				<ul className="flex gap-2">
+					<li>
+						<a
+							href="https://linkedin.com"
+							className=""
+							aria-label="Access my Linkedin profile">
+							<LinkedinLogo
+								size={32}
+								aria-hidden="true"
+							/>
+						</a>
+					</li>
+					<li>
+						<a
+							href="https://github.com/acamposlucas"
+							className=""
+							aria-label="Access my Github profile">
+							<GithubLogo
+								size={32}
+								aria-hidden="true"
+							/>
+						</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	);
+};
---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import { ProfileCard } from "../components/ProfileCard";
+
+export const Home = () => {
+	return (
+		<div className="w-11/12 max-w-7xl mx-auto my-32">
+			<section className="">
+				<ProfileCard />
+			</section>
+		</div>
+	);
+};
